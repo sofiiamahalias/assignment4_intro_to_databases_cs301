679838979a2d6d6ebb7442aa06030ecc393b6cea
Anna <tomchuukk@gmail.com>
2026-06-26 00:32:37 +0300

    create users

--- create_user.sql
+++ create_user.sql
@@ -1,15 +1,20 @@
-create user financialDirector login password 'Finance5423!';
-create user Doctor login password 'Kateryna123222@3!';
-create user Admin login password 'Hosp$$12';
+create user financialDirector with password 'Finance5423!';
+create user Doctor with password 'Kateryna123222@3!';
+create user AdminHosp with password 'Hosp$$12';
 
 grant connect on database Ass4_db to financialDirector;
 grant connect on database Ass4_db to Doctor;
-grant connect on database Ass4_db to Admin;
+grant connect on database Ass4_db to AdminHosp;
 
-grant usage on schema public to Admin;
+grant usage on schema public to AdminHosp;
 grant usage on schema public to financialDirector;
 grant usage on schema public to Doctor;
 
-grant select, insert, update on Appointments, Patients to Doctor;
-grant all privileges on all tables in schema public to Admin;
+grant select, insert, update on Appointments, Patients, Procedures, Appointemnt_Medicines to Doctor;
+grant select on Doctors, Diagnosis, Pharmacy to Doctor;
+
+grant all privileges on all tables in schema public to AdminHosp;
+grant all privileges on all sequences in schema public to AdminHosp;
+
 grant select, update on Finance to financialDirector;
+grant select on Hospitals, Locations to financialDirector;
